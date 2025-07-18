42de8eced66b6bed894ae7dd48222725d15cef4a
emduc <emilien@defiscan.info>
2025-07-18 14:59:12 +0200

    add blog posts to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next'
 import { loadReviews } from '@/lib/data/utils'
+import { posts } from '#site/content'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://defiscan.info'
@@ -30,7 +31,23 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 0.7,
     },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
   ]
+
+  // Blog pages
+  const blogPages: MetadataRoute.Sitemap = posts
+    .filter((post) => post.published)
+    .map((post) => ({
+      url: `${baseUrl}/blog/${post.slugAsParams}`,
+      lastModified: new Date(post.date),
+      changeFrequency: 'monthly' as const,
+      priority: 0.7,
+    }))
 
   // Dynamic protocol pages
   try {
@@ -45,9 +62,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       }))
     })
 
-    return [...staticPages, ...protocolPages]
+    return [...staticPages, ...blogPages, ...protocolPages]
   } catch (error) {
     console.error('Error generating sitemap:', error)
-    return staticPages
+    return [...staticPages, ...blogPages]
   }
 }
