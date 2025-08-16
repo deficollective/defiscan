a9db96aa9bfb247fc65ac698725ef9b0920f811e
emduc <emilien@defiscan.info>
2025-08-16 10:32:43 +0200

    add framework to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -44,6 +44,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 0.6,
     },
+    {
+      url: `${baseUrl}/framework`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
   ]
 
   // Blog pages
